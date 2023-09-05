658b80fb64302ec2d0aaa50fdd2d83b56724e76a
hazelnutcloud <hsnmkls@proton.me>
2023-09-05 10:59:54 +0800

    loc mvp

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,9 +1,41 @@
-use actix_web::{get, web, App, HttpServer, Responder};
+use actix_web::{error, get, web, App, HttpServer, Responder};
+use git2::Repository;
+use tokei::{Config, Languages};
+
+#[derive(serde::Serialize)]
+struct LangStat {
+    language: String,
+    code: usize,
+    comments: usize,
+    blanks: usize,
+}
 
 #[get("/repo/{owner}/{repo}/loc")]
-async fn loc(path: web::Path<(String, String)>) -> impl Responder {
+async fn loc(path: web::Path<(String, String)>) -> actix_web::Result<impl Responder> {
     let (owner, repo) = path.into_inner();
-    format!("{} {}", owner, repo)
+
+    let url = format!("https://github.com/{}/{}", owner, repo);
+    let path = format!("./repos/{}/{}", owner, repo);
+
+    Repository::clone(url.as_str(), &path).map_err(error::ErrorInternalServerError)?;
+
+    let mut languages = Languages::new();
+
+    languages.get_statistics(&[&path], &[], &Config::default());
+
+    std::fs::remove_dir_all(path).map_err(error::ErrorInternalServerError)?;
+
+    Ok(web::Json(
+        languages
+            .iter()
+            .map(|(lang, stat)| LangStat {
+                language: lang.to_string(),
+                code: stat.code,
+                comments: stat.comments,
+                blanks: stat.blanks,
+            })
+            .collect::<Vec<_>>(),
+    ))
 }
 
 #[actix_web::main]
